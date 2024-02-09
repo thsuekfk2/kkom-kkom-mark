d772ebee5a01f00ee65f8df310f9130215a75228
thsuekfk2 <thsuekfk2@naver.com>
2024-02-09 20:01:54 +0900

    feat : tailwindcss 설치 및 세팅

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Popup } from "./Popup";
+import "../../../global.css";
 
 createRoot(document.getElementById("app-container") as HTMLElement).render(
   <Popup />
