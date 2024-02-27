2791e92b2fc340e9d1b53a31abf011371602f875
thsuekfk2 <thsuekfk2@naver.com>
2024-02-28 00:09:51 +0900

    feat : chakra-ui 설치

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Popup } from "./Popup";
 import "../../../global.css";
+import { ChakraProvider, ChakraBaseProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("app-container") as HTMLElement).render(
-  <Popup />
+  <ChakraProvider>
+    <ChakraBaseProvider>
+      <Popup />
+    </ChakraBaseProvider>
+  </ChakraProvider>
 );
