f6643d4d8920fb60de83603306371aeb2e8f3f13
thsuekfk2 <thsuekfk2@naver.com>
2024-03-16 18:03:54 +0900

    feat: pagination 컴포넌트 구현

---
+++ src/components/pagination/Pagination.tsx
@@ -0,0 +1,14 @@
+import { useBookmarkListData } from "../../store/bookmark";
+import { PageItem } from "./PaginationItem";
+
+export const Pagination = () => {
+  const list = useBookmarkListData();
+
+  return (
+    <div className="flex flex-row items-center justify-center w-full gap-1 h-[60px]">
+      {list.map((_, i: number) => (
+        <PageItem page={i + 1} key={i} />
+      ))}
+    </div>
+  );
+};
---
+++ src/components/pagination/PaginationItem.tsx
@@ -0,0 +1,14 @@
+import { useActions } from "../../store/bookmark";
+
+export const PageItem = ({ page }: { page: number }) => {
+  const { current } = useActions();
+
+  return (
+    <div
+      className="flex items-center justify-center w-3 h-3 p-3 text-[10px] rounded-full cursor-pointer bg-slate-100 hover:bg-slate-200"
+      onClick={() => current.updateCurrentPage(page - 1)}
+    >
+      {page}
+    </div>
+  );
+};
