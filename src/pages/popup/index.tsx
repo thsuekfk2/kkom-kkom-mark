26a698dea391bba6944744e18d330ff482a9b56f
thsuekfk2 <thsuekfk2@naver.com>
2024-03-23 16:21:15 +0900

    style : popup html 전역 css 설정

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { Popup } from "./Popup";
 import "../../../global.css";
 import { ChakraProvider, ChakraBaseProvider } from "@chakra-ui/react";
+import "./popup.css";
 
 createRoot(document.getElementById("app-container") as HTMLElement).render(
   <ChakraProvider>
