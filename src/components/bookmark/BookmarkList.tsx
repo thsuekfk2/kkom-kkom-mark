df9040377fcda78c380c9f23072eb3e5796ff1be
thsuekfk2 <thsuekfk2@naver.com>
2024-03-23 16:32:25 +0900

    refactor : popup, bookmarkList

--- src/components/bookmark/BookmarkList.tsx
+++ src/components/bookmark/BookmarkList.tsx
@@ -1,42 +1,34 @@
 import { BookmarkItem } from "./BookmarkItem";
-import { CloseIcon } from "../Icons";
-import { bookmarkService } from "../../service/bookmark.servce";
 import {
   BookmarkListType,
-  useActions,
   useBookmarkCurrent,
   useBookmarkListData,
 } from "../../store/bookmark";
+import { useSession } from "../../store/user";
+import { DeleteBookmark } from "./DeleteBookmark";
 
 export const BookmarkList = () => {
+  const { session } = useSession();
   const { list } = useBookmarkListData();
   const { url, bookmarkPage } = useBookmarkCurrent();
-  const { updateList } = useActions();
 
-  const loadBookmarks = async () => {
-    const fetchData = await bookmarkService.fetch();
-    updateList(fetchData);
-  };
-
-  const deleteBookmark = async (url: string) => {
-    await bookmarkService.delete(url);
-    await loadBookmarks();
-  };
-
-  return list[bookmarkPage]?.map((data: BookmarkListType) => (
-    <div
-      className={`flex flex-row ${
-        url === data.url ? "bg-slate-200" : "bg-[#F1F4F6]"
-      } rounded-sm p-2 w-[90%] hover:bg-slate-200 justify-between`}
-      key={data.id}
-    >
-      <BookmarkItem data={data} />
-      <div
-        className="flex items-center p-[3px] rounded-sm cursor-pointer hover:bg-slate-100"
-        onClick={() => deleteBookmark(data.url)}
-      >
-        <CloseIcon />
-      </div>
+  return (
+    <div className="flex flex-col gap-2">
+      {session && list && list.length <= 0 && (
+        <span>즐겨찾기가 비었습니다. '+' 를 눌러 추가해보세요!</span>
+      )}
+      {list &&
+        list[bookmarkPage]?.map((data: BookmarkListType, i) => (
+          <div
+            className={`flex flex-row w-full ${
+              url === data.url ? "bg-slate-200" : "bg-[#F1F4F6]"
+            } rounded-sm p-2 hover:bg-slate-200 justify-between`}
+            key={i}
+          >
+            <BookmarkItem data={data} />
+            <DeleteBookmark data={data} />
+          </div>
+        ))}
     </div>
-  ));
+  );
 };
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
+import { LoadingIcon } from "../../components/Icons";
+import { bookmarkService } from "../../service/bookmark.service";
 import { useActions, useBookmarkCurrent } from "../../store/bookmark";
 import { Pagination } from "../../components/pagination/Pagination";
 import { BookmarkList } from "../../components/bookmark/BookmarkList";
+import { supabase } from "../../lib/supabase";
+import { Search } from "../../components/Search";
+import { AddBookMark } from "../../components/bookmark/AddBookMark";
+import { GoogleLogin } from "../../components/Auth/GoogleLogin";
+import { useSession } from "../../store/user";
+import { Logout } from "../../components/Auth/Loogout";
 
 export const Popup = () => {
   const { updateList, current } = useActions();
   const { bookmarkInfo } = useBookmarkCurrent();
+  const [initLoading, setInitLoading] = useState(true);
+  const { session, setSession } = useSession();
+
+  const getSession = async () => {
+    const { data } = await supabase.auth.getSession();
+    return data.session;
+  };
+
+  useEffect(() => {
+    (async () => {
+      chrome.storage.local.get("session", async function (result) {
+        if (result.session) {
+          await supabase.auth.setSession(result.session);
+          const session = await getSession();
+          setSession(session);
+          loadBookmarks();
+        }
+        setInitLoading(false);
+      });
+    })();
+  }, []);
 
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -20,21 +50,33 @@ export const Popup = () => {
   };
 
   return (
-    <div className="flex flex-col w-[400px] h-[450px] bg-white">
-      <div className="flex justify-center h-[60px] items-center">
-        <span className="text-[15px] text-black font-extrabold">꼼꼼마크</span>
-        <div
-          className="absolute right-0 p-1 m-3 cursor-pointer hover:bg-slate-100"
-          onClick={saveBookmark}
-        >
-          <PlusIcon height={15} width={15} />
+    <div className="flex flex-col bg-white items-center w-[400px] h-[450px] ">
+      {initLoading && (
+        <div className="absolute flex justify-center items-center bg-white w-full h-full z-10">
+          <LoadingIcon widths={24} height={24} />
         </div>
-      </div>
+      )}
+      {!session && <GoogleLogin />}
+      {session && (
+        <div className="flex flex-col  justify-center items-center h-full w-[90%] ">
+          <Logout />
+          <div className="flex h-[70px] w-full justify-center items-center ">
+            {bookmarkInfo?.url ? (
+              <div className="flex w-full flex-row justify-between">
+                description : {bookmarkInfo.description}
+              </div>
+            ) : (
+              <AddBookMark />
+            )}
+          </div>
+          <div className="h-[340px] overflow-auto w-full">
       <div className="flex flex-col h-[340px] items-center gap-2 overflow-auto">
         {list.length <= 0 && <LoadingIcon widths={24} height={24} />}
-        <BookmarkList />
-      </div>
-      <Pagination />
+            <BookmarkList />
+          </div>
+          <Pagination />
+        </div>
+      )}
     </div>
   );
 };
