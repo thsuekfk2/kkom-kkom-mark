fb5b24a79f604ea78f688d9591a8de53114fcf66
thsuekfk2 <thsuekfk2@naver.com>
2024-03-28 23:24:13 +0900

    feat : dayjs 설치, 북마크 등록 시간 계산 로직 추가

--- src/components/bookmark/BookmarkItem.tsx
+++ src/components/bookmark/BookmarkItem.tsx
@@ -1,32 +1,37 @@
-import { useState } from "react";
-import { bookmarkService } from "../../service/bookmark.service";
-import { BookmarkListType, useActions } from "../../store/bookmark";
+import { BookmarkListType } from "../../store/bookmark";
 import { ToolTip } from "../ui/ToolTip";
-import { Input } from "@chakra-ui/react";
+import dayjs from "dayjs";
 
 export const BookmarkItem = ({ data }: { data: BookmarkListType }) => {
-  const [editingData, setEditingData] = useState({ id: -1, description: "" });
-  const { updateList } = useActions();
+  const todayDate = dayjs();
+  const bookmarkDate = dayjs(data.created_at);
+  const subDay = todayDate.diff(bookmarkDate, "day");
+  const subHours = todayDate.diff(bookmarkDate, "hours");
+  const subMinute = todayDate.diff(bookmarkDate, "minute");
 
   const moveToPage = (url: string) => {
     chrome.tabs.create({ url });
   };
 
-  const loadBookmarks = async () => {
-    const { data } = await bookmarkService.fetch();
-    updateList(data);
-  };
-
-  const saveDescription = async (url: string) => {
-    await bookmarkService.updateDescription(url, editingData.description);
-    loadBookmarks();
-    setEditingData({ id: -1, description: "" });
+  const getTimeDifference = () => {
+    if (subMinute <= 0) return "방금";
+    if (subHours <= 0) return `${subMinute}분 전`;
+    if (subDay === 0 && subHours < 24) return `${subHours}시간 전`;
+    if (subDay < 7) {
+      if (subDay === 0 && todayDate.hour() < bookmarkDate.hour())
+        return `D + ${subDay}`;
+      return `D + ${subDay + 1}`;
+    }
+    return bookmarkDate.format("MM.DD");
   };
 
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row w-full h-full overflow-hidden text-nowrap">
+      <div className="min-w-[50px] text-[#8b95a1] text-[10px] flex items-center">
+        {getTimeDifference()}
+      </div>
       <div
-        className="cursor-pointer w-[140px] overflow-hidden text-nowrap text-ellipsis break-all"
+        className="cursor-pointer w-[140px] min-w-[140px]"
         onClick={() => moveToPage(data.url)}
       >
         <ToolTip label={data.url}>
